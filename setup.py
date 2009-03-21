9910626dafe3aba8d8dcbf9b8b20a50941e62be2
A. Bram Neijt <bneijt@gmail.com>
2009-03-21 16:04:06 +0100

    Release ready cleanup: v0.0.3

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(
+  name=         'autotrash',
+  version=      '0.0.3',
+  author=       'A. Bram Neijt',
+  author_email= 'bram@neijt.nl',
+  url=          'http://logfish.net/pr/autotrash/',
+  description=  'Automatic GNOME Trash purging',
+  license=      'GNU GPL v3',
+  download_url= 'http://logfish.net/pr/autotrash/downloads/',
+  long_description= 'AutoTrash is a simple Python script which will remove files which you deleted more then 30 days ago (or any other number of days you would like). It uses the FreeDesktop.org Trash Info files included in the new GNOME system to find the correct files and the dates they where deleted. ',
+  scripts= ['autotrash'],
+  )
+
