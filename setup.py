0227ff7f05b0881168d86b26dd00a541537e1bf2
A. Bram Neijt <bneijt@gmail.com>
2009-03-24 11:20:36 +0100

    Version 0.0.5 release clean-up.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
   name=         'autotrash',
-  version=      '0.0.5SVN',
+  version=      '0.0.5',
   author=       'A. Bram Neijt',
   author_email= 'bram@neijt.nl',
   url=          'http://logfish.net/pr/autotrash/',
