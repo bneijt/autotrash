fd669d35f3b5206a7b685a934c1297de6549df3c
A. Bram Neijt <bneijt@gmail.com>
2017-04-04 20:37:20 +0200

    Up the version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
   name=         'autotrash',
-  version=      '0.2.0',
+  version=      '0.2.1',
   author=       'A. Bram Neijt',
   author_email= 'bram@neijt.nl',
   url=          'http://logfish.net/pr/autotrash/',
