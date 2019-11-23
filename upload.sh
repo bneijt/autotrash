f571e25df30b6467d3537b0b38b4d174cc6cbee7
A. Bram Neijt <bram@neijt.nl>
2019-11-23 09:40:36 +0100

    Add upload switches

--- upload.sh
+++ upload.sh
@@ -2,4 +2,16 @@
 cd "$(dirname "$0")"
 rm -rf dist
 pipenv run -- python setup.py sdist bdist_wheel
-pipenv run -- python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+case $1 in
+    prod)
+        pipenv run -- python -m twine upload
+        ;;
+    test)
+        pipenv run -- python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+        ;;
+    *)
+        echo "Either prod or test"
+        exit 1
+        ;;
+esac
+
