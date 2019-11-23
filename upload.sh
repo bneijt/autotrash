e7a574c3e1e01d6e15c4cb32028cf4a718f1e936
A. Bram Neijt <bram@neijt.nl>
2019-11-23 09:53:29 +0100

    Bugfix upload script

--- upload.sh
+++ upload.sh
@@ -2,9 +2,11 @@
 cd "$(dirname "$0")"
 rm -rf dist
 pipenv run -- python setup.py sdist bdist_wheel
+echo "Ready to upload:"
+find dist
 case $1 in
     prod)
-        pipenv run -- python -m twine upload
+        pipenv run -- python -m twine upload dist/*
         ;;
     test)
         pipenv run -- python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
